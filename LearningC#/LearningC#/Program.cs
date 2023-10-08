46abd355b515faabdd2c03460d147a3a7679f195
Denislav30 <denyslav.dimitrov@gmail.com>
2023-10-08 15:06:20 +0300

    add exercise

---
+++ LearningC#/LearningC#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Lamborghini!");
+        }
+    }
+}
